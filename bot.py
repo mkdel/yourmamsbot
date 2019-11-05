f3a93994331bb4bce1bcf54e087bc36044b85dbb
Mikhail <aksenov.mikhail@gmail.com>
2019-11-05 12:24:34 +0000

    testing commint

--- bot.py
+++ bot.py
@@ -1,2 +1,39 @@
-:Wq
+# encoding=utf8
+import telepot
+import urllib3
+import time
+import secrets
+import psycopg2
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+print('\nConnecting to database')
+conn = psycopg2.connect(host="localhost",database="yourmamsbot", user="yourmamsbot", password="yourmamsbot")
+
+# Bot initialization
+token=secrets.token
+bot = telepot.Bot(token)
+
+
+def handle(msg):
+    print(msg)
+    cur = conn.cursor()
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if "text" in msg:
+        text = msg["text"]
+        insertStatement = "INSERT INTO messages (msg_id,chat_id, time, text) VALUES ('" + str(msg["message_id"]) + "','"+ str(chat_id) +"',"+ str(msg["date"]) +",'"+ text+ "');"
+#        print(insertStatement)
+        cur.execute(insertStatement)
+        conn.commit()
+        cur.close()
+
+
+bot.message_loop(handle)
+
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
--- initial.sql
+++ initial.sql
@@ -1,2 +1,6 @@
-CREATE USER yourmamsbot SUPERUSER;
+CREATE USER yourmamsbot WITH PASSWORD 'yourmamsbot';
 CREATE DATABASE yourmamsbot WITH OWNER yourmamsbot;
+\c yourmamsbot;
+CREATE TABLE messages (msg_id integer, chat_id integer, time integer, text varchar, PRIMARY KEY (msg_id, chat_id));
+ALTER TABLE messages OWNER TO yourmamsbot;
+
